aba5bad6ff40eb6aae3eec28dce5f142198c7cfe
Rina Takase <rina.m.takase@gmail.com>
2017-03-06 05:19:06 -0800

    Add name  param and bugfix

--- options.js
+++ options.js
@@ -1,10 +1,12 @@
 (function() {
   var restoreAll = function() {
     document.getElementById('callback_url').value = localStorage.getItem('callback_url');
+    document.getElementById('name').value = localStorage.getItem('name');
   };
 
   var saveAll = function() {
     localStorage.setItem('callback_url', document.getElementById('callback_url').value)
+    localStorage.setItem('name', document.getElementById('name').value)
   };
 
   var init = function() {
